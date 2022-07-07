56f9c5208ca10866c9d3ea9689de811e1bd994f8
chengqionghua <1169121469@qq.com>
2022-07-07 15:57:32 +0800

    init

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
-
   {
-    // path: '/about',
-    // name: 'About',
-    
-    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: "/",
+    name: "Index",
+
+    component: () => import("../views/Index.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
