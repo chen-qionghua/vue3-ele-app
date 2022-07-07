b7f8e3b259678812147885bddc27696beca7bba2
chengqionghua <1169121469@qq.com>
2022-07-07 16:44:23 +0800

    构建表单结构

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,6 +4,31 @@
     <div class="logo">
       <img src="../assets/logo.jpg" alt="my login image" />
     </div>
+
+    <div class="text-group">
+      <div class="input-group">
+        <input type="text" value="" placeholder="请输⼊⼿机号" />
+        <button>获取验证码</button>
+      </div>
+    </div>
+    <div class="text-group">
+      <div class="input-group">
+        <input type="text" value="" placeholder="验证码" />
+      </div>
+    </div>
+    <!-- 错误提醒 -->
+    <div v-if="false" class="invalid-feedback">报错信息</div>
+    <!-- ⽤户服务协议 -->
+    <div class="login-des">
+      <p>
+        新⽤户登录即⾃动注册，表示已同意
+        <span>《⽤户服务协议》</span>
+      </p>
+    </div>
+    <!-- 登录按钮 -->
+    <div class="login-btn">
+      <button>登录</button>
+    </div>
   </div>
 </template>
 
